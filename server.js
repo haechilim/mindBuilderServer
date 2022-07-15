2acee167aa11e29234268988827a62568927275a
haechilim <haechilim@gmail.com>
2022-07-15 21:20:24 +0900

    npm install mysql

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const mime = require('mime');
-const DatabaseManager = require('./databaseManager');
+const DatabaseManager = require('./manager/databaseManager');
 const ApiManager = require('./manager/apiManager');
 
 class Server {
