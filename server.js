d66d0c21a44f2df108325fcf6d6311d507f0bfe8
haechilim <haechilim@gmail.com>
2022-07-16 03:30:29 +0900

    test

--- server.js
+++ server.js
@@ -56,9 +56,10 @@ class Server {
 
             case "/api/healing/postModel/read":
                 this.databaseManager.postModelRead(data.id, data.readType, (error, result) => {
-                    result.forEach(element => {
+                    result.forEach(element => this.databaseManager.contentsModelRead(element.id, (error, result) => {
                         console.log(element);
-                    });
+                        if(result.indexOf(element) + 1 == result.length) this.response(response, error, result);
+                    }));
                 });
                 break;
 
